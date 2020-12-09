6c2d1e94dd9bfa896e7abf0cdce74e25d8a73f86
cookcodeblog <cookcode_me@sina.com>
2020-12-09 15:04:26 +0800

    Initial commit

---
+++ src/test/java/com/exmaple/store/StoreCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exmaple.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
