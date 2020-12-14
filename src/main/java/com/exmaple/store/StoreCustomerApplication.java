b2946b9520b27685c17533f020568d6acf37f606
Lin Sirui <lsirui@redhat.com>
2020-12-14 20:15:59 +0800

    Spring Boot version

--- src/main/java/com/exmaple/store/StoreCustomerApplication.java
+++ src/main/java/com/exmaple/store/StoreCustomerApplication.java
@@ -4,14 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
-@EnableFeignClients
-@EnableEurekaClient
 @SpringBootApplication
 public class StoreCustomerApplication {
 
@@ -19,8 +15,9 @@ public class StoreCustomerApplication {
         SpringApplication.run(StoreCustomerApplication.class, args);
     }
 
-//    @Bean
-//    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-//        return restTemplateBuilder.build();
-//    }
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
+    }
+
 }
--- src/main/java/com/exmaple/store/controller/CustomerController.java
+++ src/main/java/com/exmaple/store/controller/CustomerController.java
@@ -1,10 +1,8 @@
 package com.exmaple.store.controller;
 
 
-import com.exmaple.store.feign.PreferenceFeignClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import io.opentracing.Tracer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,34 +22,18 @@ public class CustomerController {
 
     private static final String RESPONSE_STRING_FORMAT = "customer => %s\n";
 
-//    @Autowired
-//    private RestTemplate restTemplate;
-
-//    @Value("${preferences.api.url}")
-//    private String remoteURL;
-
-//    @Autowired
-//    private Tracer tracer;
-
     @Autowired
-    private PreferenceFeignClient preferenceFeignClient;
+    private RestTemplate restTemplate;
 
+    @Value("${preferences.api.url}")
+    private String remoteURL;
 
     @HystrixCommand(fallbackMethod = "defaultGetCustomer",
             commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "4000")})
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity<String> getCustomer(@RequestHeader("User-Agent") String userAgent, @RequestHeader(value = "user-preference", required = false) String userPreference) {
         try {
-            /**
-             * Set baggage
-             */
-//            tracer.activeSpan().setBaggageItem("user-agent", userAgent);
-//            if (userPreference != null && !userPreference.isEmpty()) {
-//                tracer.activeSpan().setBaggageItem("user-preference", userPreference);
-//            }
-
-            // ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
-            ResponseEntity<String> responseEntity = preferenceFeignClient.getPreferences();
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
             String response = responseEntity.getBody();
             return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
         } catch (HttpStatusCodeException ex) {
